2915eea08978cc14edcc687dd21a956fd90a775d
Biser Nedelev <bibo0088@abv.bg>
2019-08-22 10:50:41 +0300

    Update UserService.java

--- src/main/java/org/bn/productshop/service/UserService.java
+++ src/main/java/org/bn/productshop/service/UserService.java
@@ -8,6 +8,7 @@ public interface UserService extends UserDetailsService {
 
     UserServiceModel registerUser(UserServiceModel userServiceModel);
 
+    
     UserServiceModel findUserByUserName(String username);
 
     UserServiceModel editUserProfile(UserServiceModel userServiceModel, String oldPassword);

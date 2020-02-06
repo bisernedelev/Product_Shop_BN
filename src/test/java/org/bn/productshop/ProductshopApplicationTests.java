3b631c6c4b818e860439c2a9d9de672fab4dca43
Biser Nedelev <bibo0088@abv.bg>
2020-02-06 13:02:47 -0500

    Update ProductshopApplicationTests.java

--- src/test/java/org/bn/productshop/ProductshopApplicationTests.java
+++ src/test/java/org/bn/productshop/ProductshopApplicationTests.java
@@ -7,9 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ProductshopApplicationTests {
-
-   
+public class ProductshopApplicationTests { 
    
     @Test
     public void contextLoads() {
